71249caec486ca2d05c20e2a7fc92fa7b97ecd28
0Jsnipes <jared.c.snipes@gmail.com>
2024-09-07 19:17:10 -0400

    wip

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,26 +1,74 @@
 const router = require('express').Router()
+const md = require('./accounts-middleware.js')
+const Account = require('./accounts-model.js')
 
-router.get('/', (req, res, next) => {
+router.get('/', async (req, res, next) => {
+  try {
+    const accounts = await Account.getAll()
+    res.json(accounts)
+
+  }catch(err){
+    next(err)
+  }
+  // DO YOUR MAGIC
+
+})
+
+router.get('/:id',md.checkAccountId , async (req, res, next) => {
+  try {
+    const account = await Account.getById(req.params.id0)
+    res.json(account)
+  }catch(err){
+    next(err)
+  }
   // DO YOUR MAGIC
 })
 
-router.get('/:id', (req, res, next) => {
+router.post(
+  '/', 
+  md.checkAccountPayload, 
+  md.checkAccountNameUnique,
+  async (req, res, next) => {
+  try {
+    const newAccount = await Account.create(req.body)
+    res.status(201).json(newAccount)
+  }catch(err){
+    next(err)
+  }
   // DO YOUR MAGIC
 })
 
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
+router.put(
+  '/:id',
+   md.checkAccountId, 
+   md.checkAccountPayload, 
+   md.checkAccountNameUnique, 
+   (req, res, next) => {
+  try {
 
-router.put('/:id', (req, res, next) => {
+  }catch(err){
+    next(err)
+  }
   // DO YOUR MAGIC
 });
 
-router.delete('/:id', (req, res, next) => {
+router.delete(
+  '/:id',
+  md.checkAccountId,
+   async (req, res, next) => {
+  try {
+    await Account.deleteById(req.params.id)
+    res.json(req.account)
+  }catch(err){
+    next(err)
+  }
   // DO YOUR MAGIC
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
+  res.status(err.status|| 500).json({
+    message: err.message,
+  })
   // DO YOUR MAGIC
 })
 
